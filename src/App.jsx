f2a8a9c76c4f6402af8d8a9acb0f2631201eaf23
Genene Fitsum <fitsumgenene7@gmail.com>
2025-12-12 21:20:45 +0300

    Initial commit. Organized a usable folder structure.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className = "app">
+      <h1>Welcome to G-Mart</h1>
+      <h2>An e-commerce website where you can achieve what you want.</h2>
+
+    </div>
+  )
+}
+
+export default App;
